2d916bc73134cf2d2d085bb70bcaa78f6e6bc12b
CooperAleks <cooperaleks@yahoo.com>
2015-12-09 03:04:46 +0700

    Initialize repository

---
+++ app/controllers/homepage_controller.rb
@@ -0,0 +1,20 @@
+class HomepageController < ApplicationController
+  def index
+  	@title = 'Nimbl3'
+  	@home_message = ''
+  	@faster_iterations = 'We use a pragmatic agile methodology that enables us to deliver on time, adjust quickly and reduce your time to market drastically.'
+  	@flexible_model = 'We work with you in the most convenient way for your business, either as a team addition or as your technical partner where we take care of every aspect of your product from UX/UI to infrastructure maintenance.'
+  	@transparency = 'As a client you are at the center of the process providing direct feedback in the planning, feature development and product operations.'
+  	@mvpforstartups = 'Let us become your tech team to build your Minimum Viable Product. Go fast your idea to the launch of a solid and beautiful product to the market.'
+  	@rubyonrails = 'We specialize in building scalable, secure and robust Ruby on Rails applications. We have expertise in several vertical such as e-commerce and consumer facing products.'
+  	@mobileapps = 'We would lover to build your native iOS or Android mobile application that brings your product experience to the hands of your users.'
+  	@sos = 'Our expertise in building, scaling and securing web and mobile application architectures, have helped our clients to delight their users.'
+  	@olivierrobert = 'Developer and problem solver with strong experience in early-stage start-ups. Raised on a French remote island, he is now almost a local in Thailand.'
+  	@team_desc = 'Developer and problem solver with strong experience in early-stage start-ups. Raised on a French remote island, he is now almost a local in Thailand.'
+  	@jeremirenner = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque aliquam in arcu malesuada pretium. Integer non ipsum lectus. Duis sit amet tellus sed risus ullamcorper pulvinar. Nulla facilisi. Donec pellentesque erat nec scelerisque viverra. Aliquam rutrum lacus in quam iaculis, mattis rhoncus dolor condimentum. Aenean fringilla nibh sed fermentum ullamcorper.'
+  	@founder = "Co Fonder"
+  	@dev = "Developer"
+  	@softeng = "Software engineer"
+  	@des = "Designer"
+  end
+end
